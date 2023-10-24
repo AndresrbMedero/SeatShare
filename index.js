c4cbb5d9bc29dd708760275882f75d6721be0f39
Andres Medero <andresr.bmedero@gmail.com>
2023-10-24 17:27:02 -0600

    cambios sin importancia

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hola, mundo');
+  res.end('SeatShare');
 });
 
 server.listen(3000, () => {
-  console.log('El servidor está escuchando en el puerto 3000');
+  console.log('El servidor está en http://localhost:3000');
 });
